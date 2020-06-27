c1540f72ddc6222265066ce8087ca50b6ea64e26
rmelikov <48834507+rmelikov@users.noreply.github.com>
2020-06-27 09:41:22 -0500

    up

--- ch2/applied.R
+++ ch2/applied.R
@@ -11,27 +11,28 @@ summary(college)
 # ii.
 pairs(college[,1:10])
 # iii.
-plot(college$Private, college$Outstate)
+attach(college)
+plot(Private, Outstate)
 # iv.
 Elite = rep("No", nrow(college))
-Elite[college$Top10perc>50] = "Yes"
+Elite[Top10perc>50] = "Yes"
 Elite = as.factor(Elite)
 college = data.frame(college, Elite)
-summary(college$Elite)
-plot(college$Elite, college$Outstate)
+summary(Elite)
+plot(Elite, Outstate)
 # v.
 par(mfrow=c(2,2))
-hist(college$Apps)
-hist(college$perc.alumni, col=2)
-hist(college$S.F.Ratio, col=3, breaks=10)
-hist(college$Expend, breaks=100)
+hist(Apps)
+hist(perc.alumni, col=2)
+hist(S.F.Ratio, col=3, breaks=10)
+hist(Expend, breaks=100)
 # vi.
 par(mfrow=c(1,1))
-plot(college$Outstate, college$Grad.Rate)
+plot(Outstate, Grad.Rate)
 # High tuition correlates to high graduation rate.
-plot(college$Accept / college$Apps, college$S.F.Ratio)
+plot(Accept / Apps, S.F.Ratio)
 # Colleges with low acceptance rate tend to have low S:F ratio.
-plot(college$Top10perc, college$Grad.Rate)
+plot(Top10perc, Grad.Rate)
 # Colleges with the most students from top 10% perc don't necessarily have
 # the highest graduation rate. Also, rate > 100 is erroneous!
 
